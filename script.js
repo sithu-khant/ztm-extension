7621facc934d51e5c0375f2f61d2507b131eca5b
Sithu Khant <sithu.khantht@gmail.com>
2023-12-12 08:30:36 +0630

    updated

--- script.js
+++ script.js
@@ -31,23 +31,34 @@ lectureLeft.parentNode.insertBefore(addZtmCheckbox, lectureLeft.nextSibling);
 
 var ztmCheckbox = document.getElementById('ztm-toggle-hide');
 var courseSidebar = document.getElementById('courseSidebar');
-var lectureVideo = document.getElementsByClassName('full-width-content')[0]
+
+function ztmToggleSidebar() {
+    var lectureVideo = document.querySelector('.course-mainbar.lecture-content');
+
+    // if checked, hide sidebar
+    if (ztmCheckbox.checked) {
+        courseSidebar.style.transition = "transform 0.3s";
+        courseSidebar.style.transform = 'translateX(-100%)'
+
+        lectureVideo.style.marginLeft = '0';
+        lectureVideo.style.transition = "all 0.3s";
+    } else {
+        courseSidebar.style.transition = "transform 0.3s";
+        courseSidebar.style.transform = 'translateX(0%)';
+
+        lectureVideo.style.marginLeft = '';
+        lectureVideo.style.transition = "all 0.3s";
+    };
+};
+
 
 // hide sidebar
-ztmCheckbox.addEventListener('change', function () {
+ztmCheckbox.addEventListener('change', ztmToggleSidebar);
+ztmToggleSidebar()
 
-	// if checked, hide sidebar
-	if (ztmCheckbox.checked) {
-		courseSidebar.style.transition = "transform 0.3s";
-		courseSidebar.style.transform = 'translateX(-100%)'
 
-		lectureVideo.style.marginLeft = '0';
-		lectureVideo.style.transition = "all 0.3s";
-	} else {
-		courseSidebar.style.transition = "transform 0.3s";
-		courseSidebar.style.transform = 'translateX(0%)';
 
-		lectureVideo.style.marginLeft = '';
-		lectureVideo.style.transition = "all 0.3s";
-	};
-})
+
+
+
+
