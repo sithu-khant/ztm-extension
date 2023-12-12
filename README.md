4a0f89951afe635d3532d2e1f7300c492c44934e
Sithu Khant <sithu.khantht@gmail.com>
2023-12-12 11:06:41 +0630

    finished darkmode section

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# ztm-toggle-bar
+# Zero To Mastery - Extension
 
 * Sun Dec 10, 2023 - Started the project.
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var lectureLeft = document.querySelector('.nav-icon-back');
-var addZtmCheckbox = document.createElement('div');
+// ----------
+// Start Togglebar Section
+// ----------
 
-addZtmCheckbox.innerHTML = `
+var lectureLeft = document.querySelector('.nav-icon-back');
+var addZtmToggleCheckbox = document.createElement('div');
+
+addZtmToggleCheckbox.innerHTML = `
 <!-- ZTM Toggle Bar by Sithu Khant -->
 
 <style type="text/css">
@@ -27,16 +31,17 @@ addZtmCheckbox.innerHTML = `
 <!-- ZTM Toggle Bar by Sithu Khant -->
 `;
 
-lectureLeft.parentNode.insertBefore(addZtmCheckbox, lectureLeft.nextSibling);
+// add toggle checkbox after back-to-home icon
+lectureLeft.parentNode.insertBefore(addZtmToggleCheckbox, lectureLeft.nextSibling);
 
-var ztmCheckbox = document.getElementById('ztm-toggle-hide');
+var ztmToggleCheckbox = document.getElementById('ztm-toggle-hide');
 var courseSidebar = document.getElementById('courseSidebar');
 
 function ztmToggleSidebar() {
     var lectureVideo = document.querySelector('.course-mainbar.lecture-content');
 
     // if checked, hide sidebar
-    if (ztmCheckbox.checked) {
+    if (ztmToggleCheckbox.checked) {
         courseSidebar.style.transition = "transform 0.3s";
         courseSidebar.style.transform = 'translateX(-100%)'
 
@@ -52,7 +57,7 @@ function ztmToggleSidebar() {
 };
 
 // hide sidebar
-ztmCheckbox.addEventListener('change', ztmToggleSidebar);
+ztmToggleCheckbox.addEventListener('change', ztmToggleSidebar);
 
 // Use MutationObserver to detect changes and apply styles
 var observer = new MutationObserver(function (mutations) {
@@ -73,6 +78,104 @@ var observer = new MutationObserver(function (mutations) {
 var observerConfig = { childList: true, subtree: true };
 observer.observe(document.body, observerConfig);
 
+// ----------
+// End Togglebar Section
+// ----------
+
+// ----------
+// Start Darkmode Section
+// ----------
+
+
+var dropdownMenuUl = document.querySelector('.dropdown-menu');
+var addZtmDrakmodeLi = document.createElement('span');
+var addZtmDrakmodeStyle = document.createElement('div');
+
+addZtmDrakmodeLi.innerHTML = `
+<!-- ZTM Darkmode by Sithu Khant -->
+
+<li class="nav-focus" aria-label="menuitem">
+    <div class="switch" id="switch-autoplay-lectures">
+        <input id="ztm-darkmode" class="custom-toggle custom-toggle-round" type="checkbox">
+        <label for="ztm-darkmode"></label>
+    </div>
+    <span>Darkmode</span>
+</li>
+
+<!-- ZTM Darkmode by Sithu Khant -->
+`;
+
+addZtmDrakmodeStyle.innerHTML = `
+<!-- ZTM Darkmode Style by Sithu Khant -->
+
+<style type="text/css">
+    body,
+    div,
+    ul, li, .item {
+        background-color: #1E1E1E !important;
+        color: #fff !important;
+    }
+
+    a {
+        color: #fff !important;
+    }
+
+    input.custom-toggle-round:checked+label:before {
+        background: #4e4e4e !important;
+        border: 1px solid #4e4e4e;
+    }
+
+    input.custom-toggle-round:checked+label:after {
+        background: #fff !important;
+        border: 1px solid #4e4e4e;
+    }
+
+    #settings_menu:hover, .nav-btn:hover {
+        background-color: #4e4e4e !important;
+    }
+
+    header.half-height .lecture-left .open .dropdown-toggle {
+        background-color: #4e4e4e !important;
+    }
+
+    .nav-btn.complete,
+    .progressbar .progressbar-fill {
+        background-color: #4e4e4e !important;
+    }
+
+    .course-sidebar .course-progress.lecture-page.sticky {
+        background: #1E1E1E !important;
+    }
+
+    .course-sidebar .row.lecture-sidebar .course-section .item:hover,
+    .course-sidebar .row.lecture-sidebar .course-section .section-item .title-container:hover {
+        background-color: #4e4e4e !important;
+    }
+
+</style>
+
+<!-- ZTM Darkmode Style by Sithu Khant -->
+`;
+
+// append the darkmode style li to ul
+dropdownMenuUl.appendChild(addZtmDrakmodeLi);
+
+var ztmToggleDarkmodeCheckbox = document.getElementById('ztm-darkmode');
+
+function ztmToggleDarkmode() {
+    if (ztmToggleDarkmodeCheckbox.checked) {
+        // append the darkmode style li to ul
+        dropdownMenuUl.appendChild(addZtmDrakmodeStyle);
+    } else {
+       dropdownMenuUl.removeChild(addZtmDrakmodeStyle);
+    };
+};
+
+ztmToggleDarkmodeCheckbox.addEventListener('change', ztmToggleDarkmode);
+
+// ----------
+// End Darkmode Section
+// ----------
 
 
 
