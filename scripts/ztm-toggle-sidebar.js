837e8c3ef88ce5c976852ac5aed45c452d072a8a
Sithu Khant <122719409+sithu-khant@users.noreply.github.com>
2024-01-12 18:30:22 +0630

    1.12.02

--- scripts/ztm-toggle-sidebar.js
+++ scripts/ztm-toggle-sidebar.js
@@ -16,66 +16,47 @@ createZtmToggleSidebar.innerHTML = `
 
 const homeBackIcon = document.querySelector('.nav-icon-back');
 
-// Only add the toggle sidebar feature if there is `homeBackIcon`
+// Check the `homeBackIcon` to prevent conflix
 if (homeBackIcon) {
+    // Add ZTM Toggle
     homeBackIcon.parentNode.insertBefore(createZtmToggleSidebar, homeBackIcon.nextSibling);
-
+    // Get the ZTM Toggle Checkbox
     const ztmToggleCheckbox = document.getElementById('ztm-toggle-checkbox');
-    const courseSidebar = document.getElementById('courseSidebar');
-    const lectureVideo = document.querySelector('.course-mainbar.lecture-content');
 
     // Create objects for hiding/showing the sidebar
     const hideSidebarStyles = { transform: 'translateX(-100%)' };
     const showSidebarStyles = { transform: 'translateX(0%)', transition: 'transform 0.3s' }
 
-    // Create objects for hiding/showing the lectureVideo
-    const addLectureVideoStyles = { marginLeft: '0' };
-    const removeLectureVideoStyles = { marginLeft: '', transition: 'all 0.3s'}
+    // Create objects for hiding/showing the lectureContent
+    const addLectureContentStyles = { marginLeft: '0' };
+    const removeLectureContentStyles = { marginLeft: '', transition: 'all 0.3s'}
 
     // Get the sidebar status 
     // (this allows the checkbox is still checked even if the page is reloaded)
     const ztmToggleSidebarCheckStatus = localStorage.getItem('ztmToggleSidebarStatus') === 'true';
     ztmToggleCheckbox.checked = ztmToggleSidebarCheckStatus;
 
-    const applyToggleSidebarStyles = () => {
+    const ztmToggleSidebar = () => {
+        const courseSidebar = document.getElementById('courseSidebar');
+        const lectureContent = document.querySelector('.course-mainbar.lecture-content');
+
         // Assign the styles based on the check status
         const sidebarStyles = ztmToggleCheckbox.checked ? hideSidebarStyles : showSidebarStyles;
-        const lectureVideoStyles = ztmToggleCheckbox.checked ? addLectureVideoStyles : removeLectureVideoStyles;
+        const lectureContentStyles = ztmToggleCheckbox.checked ? addLectureContentStyles : removeLectureContentStyles;
         
-        // Apply styles to courseSidbar and lectureVideo
+        // Apply styles to courseSidbar and lectureContent
         Object.assign(courseSidebar.style, sidebarStyles);
-        Object.assign(lectureVideo.style, lectureVideoStyles);
-    };
+        Object.assign(lectureContent.style, lectureContentStyles);
 
-    const ztmToggleSidebar = () => {
-        // Add styles to the sidebar
-        applyToggleSidebarStyles();
         localStorage.setItem('ztmToggleSidebarStatus', ztmToggleCheckbox.checked)
     };
 
     ztmToggleCheckbox.addEventListener('change', ztmToggleSidebar);
 
-    // Observering the page everytime and adding styles
-    const ztmToggleSidebarObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            console.log('mutation looping...')
-            if (mutation.addedNotes) {
-                console.log('addedNotes working')
-                mutation.addedNotes.forEach((node) => {
-                    // check if there is `lecture-content` div
-                    if (node.classList?.contains('lecture-content')) {
-                        applyToggleSidebarStyles()
-                        console.log('final working')
-                    };
-                });
-            };
-        });
-    });
-
+    // Track the page for every new lecture  
+    const ztmToggleSidebarObserver = new MutationObserver(() => ztmToggleSidebar());
     ztmToggleSidebarObserver.observe(document.body, { childList: true, subtree: true });
-
     ztmToggleSidebar();
-
 }
 
 
