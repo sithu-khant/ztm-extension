fa789d11b7c64090a36e857077a68dc6fbc406b4
Kevin Batdorf <kbat82@gmail.com>
2024-07-10 16:24:36 +0700

    Include course list details

--- .github/scripts/scrape-courses.ts
+++ .github/scripts/scrape-courses.ts
@@ -1,37 +1,96 @@
 import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts'
 
+// This is the schema for the course list items we persist
 type CourseListItem = {
     id: string
     name: string
     description: string
-    link: string
-    author: string
+    instructor: string
     thumbnail: string
+    link: string | null
+    courseLink: string | null
+    length: string | null
+    lessons: number | null
+    level: string | null
 }
 
-// There's a script tag in the dom with updated URL redirects
-const extractChangedUrls = (document: any): Record<string, string> => {
-    const scriptContent = [...document.querySelectorAll('script')].find(
-        (script: any) =>
-            script.textContent &&
-            script.textContent.includes('const changed_urls')
-    )
+// This is the type for additional details we get from the details page
+// The courses list has more courses on it, but less details
+type CourseDescriptionItem = {
+    id: string
+    level: string
+    name: string
+    description: string
+    detailsLink: string
+    courseLink: string
+    length: string
+    lessons: number
+    instructor: string
+}
 
-    const match = scriptContent.textContent?.match(
-        /const\s+changed_urls\s*=\s*(\{[\s\S]*?\})\s*;/
+// This page has more updated information, but doesn't include every available "course".
+// This list isn't paginated so we can grab all the courses at once.
+const getCourseDescriptions = async (): Promise<CourseDescriptionItem[]> => {
+    console.log('Fetching course details...')
+    const response = await fetch('https://zerotomastery.io/courses/')
+    const document = new DOMParser().parseFromString(
+        await response.text(),
+        'text/html'
     )
-    if (!match) {
-        throw new Error('Changed URLs object could not be extracted.')
+    const foundCourses = [
+        ...document.querySelectorAll('[class*="CourseCardsSection"] > div'),
+    ]
+    if (!foundCourses.length) {
+        throw new Error('No courses found on the page.')
     }
 
-    // Sanitize and convert to JSON before parsing
-    const jsonString = match[1]
-        .replace(/(\w+):/g, '"$1":') // Quote property names
-        .replace(/'/g, '"') // Replace single quotes with double quotes
-        .replace(/,\s*}/g, '}')
+    return foundCourses.map((node) => {
+        // The level is the first dom element and has an inline color
+        const level = node.querySelector('div[color]').textContent.trim()
+        const name = node
+            .querySelector('a[class*=CardTitle]')
+            .textContent.trim()
+        const description = node
+            .querySelector('p[class*=Excerpt]')
+            .textContent.trim()
+        const courseLink = node
+            .querySelector(
+                'a[href^="https://academy.zerotomastery.io/courses"]'
+            )
+            .getAttribute('href')
+        const id = String(courseLink.match(/courses\/(\d+)\/lectures/)?.[1])
+        const detailsLink =
+            'https://zerotomastery.io' +
+            node.querySelector('a[href^="/courses"]').getAttribute('href')
+        // Split the span to get the time and lessons
+        const meta = node
+            .querySelector('span[class*=TimeDetails]')
+            .textContent.split('•')
+        if (meta.length !== 2) {
+            console.error({ name, meta })
+            throw new Error('Unexpected meta length')
+        }
+        const length = meta[0].trim()
+        const lessons = parseInt(meta[1].trim().split(' ')[0], 10)
+        const instructor = node
+            .querySelector('span[class*="AuthorName"]')
+            .textContent.trim()
 
-    return JSON.parse(jsonString)
+        return {
+            id,
+            name,
+            description,
+            detailsLink,
+            courseLink,
+            level,
+            length,
+            lessons,
+            instructor,
+        }
+    })
 }
+
+const courseDescriptions = await getCourseDescriptions()
 
 const fetchAndParse = async (
     url: string,
@@ -52,31 +111,50 @@ const fetchAndParse = async (
         throw new Error('No courses found on the page.')
     }
 
-    // Some of the url slugs have changed and appear in the document
-    const changedUrls = extractChangedUrls(document)
-
     const courses = foundCourses.map((node) => {
-        const id = node.getAttribute('data-course-id')
-        const href = node.getAttribute('data-course-url')
-        // Check if the URL has changed
-        // If not, then replace the /p/ with courses/
-        const link = `https://zerotomastery.io/${
-            changedUrls[href] ?? href.replace('/p/', 'courses/')
-        }`
+        const id = String(node.getAttribute('data-course-id'))
+        const name = String(
+            node.querySelector('.course-listing-title').textContent.trim()
+        )
+        // Get the details by matching id or name
+        const details =
+            courseDescriptions.find((c) => c.id === id) ??
+            courseDescriptions.find((c) => c.name === name)
 
-        const thumbnail = node
-            .querySelector('.course-box-image')
-            .getAttribute('src')
-        const name = node
-            .querySelector('.course-listing-title')
-            .textContent.trim()
-        const description = node
-            .querySelector('.course-listing-subtitle')
-            .textContent.trim()
-        const author = node
-            .querySelector('.course-author-name')
-            .textContent.trim()
-        return { id, name, description, link, author, thumbnail }
+        const thumbnail = String(
+            node.querySelector('.course-box-image').getAttribute('src')
+        )
+        const description =
+            details?.description ??
+            String(
+                node
+                    .querySelector('.course-listing-subtitle')
+                    .textContent.trim()
+            )
+        const instructor =
+            details?.instructor ??
+            String(node.querySelector('.course-author-name').textContent.trim())
+        const link = details?.detailsLink ?? null
+        const courseLink = details?.courseLink ?? null
+
+        // Set missing details to null
+        const { length, lessons, level } = details ?? {
+            length: null,
+            lessons: null,
+            level: null,
+        }
+        return {
+            id,
+            name,
+            description,
+            instructor,
+            thumbnail,
+            link,
+            courseLink,
+            length,
+            lessons,
+            level,
+        }
     })
 
     // Find the next page URL if it exists
--- scripts/ztm-course-details.js
+++ scripts/ztm-course-details.js
@@ -1,6 +1,6 @@
-/* 
+/*
  * Author: Sithu Khant
- * GitHub: https://github.com/sithu-khant 
+ * GitHub: https://github.com/sithu-khant
  * Last Updated: Wed Jun 5, 2024
  * Description: Adds course details feature to the home page
  */
@@ -8,20 +8,20 @@
 // Create Box icons style
 const courseDetailsBoxIconsCss = document.createElement('link')
 courseDetailsBoxIconsCss.rel = 'stylesheet'
-courseDetailsBoxIconsCss.href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
+courseDetailsBoxIconsCss.href =
+    'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
 
 // Add Box icons style to the head
 document.head.appendChild(courseDetailsBoxIconsCss)
 
 // Fav courses components
 const courseDetailsComponents = () => {
-
     // Get course cards
     let courseCards = document.querySelectorAll('.course-listing')
 
     // Add heart icon to each courses
     courseCards.forEach((courseCard) => {
-        // get the course 
+        // get the course
         const courseCardRow = courseCard.querySelector('.row')
 
         // Create a new div
@@ -33,7 +33,7 @@ const courseDetailsComponents = () => {
         // Append `ztmCourseDetailsAnchor` to `ztmCourseDetailsDiv`
         ztmCourseDetailsDiv.appendChild(ztmCourseDetailsAnchor)
 
-        // Create heart icon element 
+        // Create heart icon element
         // <i class='bx bxs-heart' id='ztm-heart'></i>
         let ztmCourseDetailsIcon = document.createElement('i')
         ztmCourseDetailsIcon.classList.add('bx', 'bxs-info-circle')
@@ -49,9 +49,12 @@ const courseDetailsComponents = () => {
 const courseDetailsIconPosition = () => {
     // Get the favourite course feature icon status
     const favoriteCoursesFeature = document.getElementById('ztm-fav-courses')
-    const isFavoriteCoursesFeature = favoriteCoursesFeature?.getAttribute('style')
+    const isFavoriteCoursesFeature =
+        favoriteCoursesFeature?.getAttribute('style')
 
-    const ztmCourseDetailsDivs = document.querySelectorAll('#ztm-course-details')
+    const ztmCourseDetailsDivs = document.querySelectorAll(
+        '#ztm-course-details'
+    )
 
     ztmCourseDetailsDivs.forEach((div) => {
         // If favorite course feature display is none, make ztmCourseDetailsIcon `right: 2%`
@@ -68,23 +71,42 @@ const courseDetails = () => {
     courseDetailsIconPosition()
 
     // Track the page for favorite courses feature status
-    const observeCourseDetails = new MutationObserver(() => courseDetailsIconPosition())
-    observeCourseDetails.observe(document.body, { childList: true, subtree: true })
+    const observeCourseDetails = new MutationObserver(() =>
+        courseDetailsIconPosition()
+    )
+    observeCourseDetails.observe(document.body, {
+        childList: true,
+        subtree: true,
+    })
 
     // Get all the course details icon
-    let ztmCourseDetailsIcons = document.querySelectorAll('#ztm-course-details-icon')
+    let ztmCourseDetailsIcons = document.querySelectorAll(
+        '#ztm-course-details-icon'
+    )
 
-    ztmCourseDetailsIcons.forEach(courseDetailsIcon => {
+    ztmCourseDetailsIcons.forEach((courseDetailsIcon) => {
         // Get the course data
-        const courseCard = courseDetailsIcon.closest('.col-xs-12.col-sm-6.col-md-4')
-        const courseTitle = courseCard.querySelector('.course-listing-title').textContent.trim()
+        const courseCard = courseDetailsIcon.closest(
+            '.col-xs-12.col-sm-6.col-md-4'
+        )
+        const courseTitle = courseCard
+            .querySelector('.course-listing-title')
+            .textContent.trim()
         // Get `ztm-course-details-anchor` tag
-        const ztmCourseDetailsAnchor = courseDetailsIcon.closest('#ztm-course-details-anchor')
+        const ztmCourseDetailsAnchor = courseDetailsIcon.closest(
+            '#ztm-course-details-anchor'
+        )
 
         // Course title data cleaning
         const removeColonFromTitle = courseTitle.replace(/:/g, '')
-        const removeBracketsFromTitle = removeColonFromTitle.replace(/[()]/g, '')
-        const removeYearsFromTitle = removeBracketsFromTitle.replace(/\b\d{4}\b/g, '')
+        const removeBracketsFromTitle = removeColonFromTitle.replace(
+            /[()]/g,
+            ''
+        )
+        const removeYearsFromTitle = removeBracketsFromTitle.replace(
+            /\b\d{4}\b/g,
+            ''
+        )
         const removeInFromTitle = removeYearsFromTitle.replace('in', '')
         // Cleaned titleArray
         const titleArray = removeInFromTitle.split(' ')
@@ -93,21 +115,32 @@ const courseDetails = () => {
         const jsonUrl = chrome.runtime.getURL('../course-details.json')
 
         fetch(jsonUrl)
-            .then(res => res.json())
+            .then((res) => res.json())
             .then((responses) => {
                 responses.forEach((res) => {
-                    const removeColonFromResTitleArray = res.name?.replace(/:/g, '')
-                    const removeBracketsFromResTitleArray = removeColonFromResTitleArray.replace(/[()]/g, '')
-                    const removeYearsFromResTitleArray = removeBracketsFromResTitleArray.replace(/\b\d{4}\b/g, '')
-                    const removeInFromResTitleArray = removeYearsFromResTitleArray.replace('in', '')
+                    const removeColonFromResTitleArray = res.name?.replace(
+                        /:/g,
+                        ''
+                    )
+                    const removeBracketsFromResTitleArray =
+                        removeColonFromResTitleArray.replace(/[()]/g, '')
+                    const removeYearsFromResTitleArray =
+                        removeBracketsFromResTitleArray.replace(
+                            /\b\d{4}\b/g,
+                            ''
+                        )
+                    const removeInFromResTitleArray =
+                        removeYearsFromResTitleArray.replace('in', '')
                     // Cleaned resTitleArray
                     const resTitleArray = removeInFromResTitleArray.split(' ')
 
                     const courseDetailsUrl = res.link
                     // Check all the element in resTitleArray exists in titleArray or not
-                    const isTrue = resTitleArray.every(element => titleArray.includes(element))
+                    const isTrue = resTitleArray.every((element) =>
+                        titleArray.includes(element)
+                    )
 
-                    if (isTrue) {
+                    if (isTrue && courseDetailsUrl) {
                         // Add courseDetailsUrl as url link
                         ztmCourseDetailsAnchor.href = courseDetailsUrl
                     }
@@ -119,9 +152,13 @@ const courseDetails = () => {
                 })
 
                 // Remove course details feature those don't have url
-                const getCourseDetailsAnchor = courseDetailsIcon.closest('#ztm-course-details-anchor')
+                const getCourseDetailsAnchor = courseDetailsIcon.closest(
+                    '#ztm-course-details-anchor'
+                )
                 if (!getCourseDetailsAnchor.href) {
-                    const getCourseDetails = courseDetailsIcon.closest('#ztm-course-details')
+                    const getCourseDetails = courseDetailsIcon.closest(
+                        '#ztm-course-details'
+                    )
                     getCourseDetails.style.display = 'none'
                     getCourseDetails.remove()
                 }
@@ -131,15 +168,19 @@ const courseDetails = () => {
 
 const toggleCourseDetails = (isChecked) => {
     // Get all the course details div
-    const ztmCourseDetailsDivs = document.querySelectorAll('#ztm-course-details')
-    ztmCourseDetailsDivs.forEach(div => {
+    const ztmCourseDetailsDivs = document.querySelectorAll(
+        '#ztm-course-details'
+    )
+    ztmCourseDetailsDivs.forEach((div) => {
         div.style.display = isChecked ? 'block' : 'none'
     })
 
     // Get all the course-listing
     let courseListing = document.querySelectorAll('.course-listing')
     courseListing.forEach((course) => {
-        const ztmCourseDetailsDivs = course.querySelectorAll('#ztm-course-details')
+        const ztmCourseDetailsDivs = course.querySelectorAll(
+            '#ztm-course-details'
+        )
 
         // Remove extra div starting from the first one up to the second-to-last one
         for (let i = 0; i < ztmCourseDetailsDivs.length - 1; i++) {
@@ -159,8 +200,12 @@ const ztmCourseDetails = () => {
 
     // Get the checkbox status dynamically and apply style
     chrome.storage.onChanged.addListener((changes, namespace) => {
-        if (namespace === 'sync' && 'ztmCourseDetailsCheckboxIsChecked' in changes) {
-            let isChecked = changes.ztmCourseDetailsCheckboxIsChecked.newValue || false
+        if (
+            namespace === 'sync' &&
+            'ztmCourseDetailsCheckboxIsChecked' in changes
+        ) {
+            let isChecked =
+                changes.ztmCourseDetailsCheckboxIsChecked.newValue || false
             courseDetails()
             toggleCourseDetails(isChecked)
         }
